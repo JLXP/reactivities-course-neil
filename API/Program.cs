8959882f3d78a711f48f2f46912ba0c1ed54f055
JavierXool <javier.xool@outlook.com>
2025-08-17 09:06:07 -0600

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt=>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+/*
+
+CreateScope() → es como decir:
+“Voy a armar un mini-equipo temporal dentro de la oficina principal para hacer un trabajo en particular.”
+
+Ese equipo solo existe mientras dure la tarea, y después se disuelve.
+
+using var scope = ... → significa que creaste ese mini-equipo (scope), lo usas por un ratito y cuando terminas, automáticamente se despide y libera todo lo que estaba ocupando.*/
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
+app.Run();
