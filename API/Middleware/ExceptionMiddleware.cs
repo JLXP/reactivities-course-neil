efd167e80c689c1c784d5a08801743d2a67dae03
JavierXool <javier.xool@outlook.com>
2025-08-28 08:49:00 -0600

    Handling exceptions

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,24 +1,53 @@
+using System.Text.Json;
+using Application.Core;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace API.Middleware;
 
 public class ExceptionMiddleware : IMiddleware
 {
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    private readonly IHostEnvironment _env;
+
+    public ExceptionMiddleware(ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+    {
+        _logger = logger;
+        _env = env;
+    }
+
+
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         try
         {
             await next(context);
         }
-        catch(ValidationException ex)
+        catch (ValidationException ex)
         {
             await HandleValidationException(context, ex);
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
+            await HandleException(context, ex);
         }
+    }
+
+    private async Task HandleException(HttpContext context, Exception ex)
+    {
+        _logger.LogError(ex, ex.Message);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+        var response = _env.IsDevelopment()
+            ? new AppException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+            : new AppException(context.Response.StatusCode, ex.Message, null);
+
+
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        var json = JsonSerializer.Serialize(response, options);
+        await context.Response.WriteAsync(json);
     }
 
     private static async Task HandleValidationException(HttpContext context, ValidationException ex)
