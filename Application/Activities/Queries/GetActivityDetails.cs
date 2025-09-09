8d5e53eb116d0ca01b1b76a190a64d4ebc3e91b1
JavierXool <javier.xool@outlook.com>
2025-09-09 17:03:19 -0600

    Loading related data

--- Application/Activities/Queries/GetActivityDetails.cs
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -2,6 +2,7 @@ using System;
 using Application.Core;
 using Domain;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities.Queries;
@@ -25,7 +26,11 @@ public class GetActivityDetails
 
         public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
-            var activity = await _context.Activities.FindAsync([request.Id], cancellationToken);
+            var activity = await _context.Activities
+            .Include(x => x.Attendees)
+            .ThenInclude(x => x.User)
+            .FirstOrDefaultAsync(x => request.Id == x.Id, cancellationToken);
+
             if (activity == null) Result<Activity>.Failure("Activity not found", 404);
             return Result<Activity>.Success(activity!);
         }
