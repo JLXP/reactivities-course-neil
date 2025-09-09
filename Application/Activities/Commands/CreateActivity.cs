53d24cbbf8ee0a360fd4ef99255bd4cac85cdfd6
JavierXool <javier.xool@outlook.com>
2025-09-09 15:14:15 -0600

    Updating the Create Activity

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,6 +1,7 @@
 using System;
 using Application.Activities.DTO;
 using Application.Core;
+using Application.Interfaces;
 using AutoMapper;
 using Domain;
 using FluentValidation;
@@ -21,22 +22,37 @@ public class CreateActivity
     {
         private readonly AppDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IUserAccessor _userAccessor;
 
-        public Handler(AppDbContext context, IMapper mapper)
+        public Handler(AppDbContext context, IMapper mapper, IUserAccessor userAccessor)
         {
             _context = context;
             _mapper = mapper;
+            _userAccessor = userAccessor;
 
         }
 
         public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
 
+            var user = await _userAccessor.GetUserAsync();
+
             var activity = _mapper.Map<Activity>(request.ActivityDTO);
             _context.Activities.Add(activity);
-            var result = await _context.SaveChangesAsync(cancellationToken)>0;
 
-            if(!result) return Result<string>.Failure("Failed to create activity", 400);
+            var attendee = new ActivityAttendee
+            {
+                ActivityId = activity.Id,
+                UserId = user.Id,
+                IsHost = true
+            };
+
+            activity.Attendees.Add(attendee);
+
+
+            var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result) return Result<string>.Failure("Failed to create activity", 400);
             return Result<string>.Success(activity.Id);
         }
 
