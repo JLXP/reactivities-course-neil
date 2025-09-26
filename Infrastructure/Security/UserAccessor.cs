d496cb184d75a2fd3200cb2777231d2652da7a71
JavierXool <javier.xool@outlook.com>
2025-09-25 23:41:11 -0600

    Deleting photos

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -2,6 +2,7 @@ using System.Security.Claims;
 using Application.Interfaces;
 using Domain;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security;
@@ -16,5 +17,16 @@ public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext
     public string GetUserId()
     {
         return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("No user found");
+    }
+
+    public async Task<User> GetUserWithPhotoAsync()
+    {
+        var userId = GetUserId();
+
+
+        return await dbContext.Users
+            .Include(x => x.Photos)
+            .FirstOrDefaultAsync(x=>x.Id == userId)
+             ?? throw new UnauthorizedAccessException("No user is logged in");
     }
 }
