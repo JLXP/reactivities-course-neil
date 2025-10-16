34fe5133b476d85ddbded508fa135aa7d4eece73
JavierXool <javier.xool@outlook.com>
2025-10-16 13:14:53 -0600

    Updating the profile class and mapping profiles part2

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -16,15 +16,15 @@ public class MappingProfiles : Profile
         CreateMap<EditActivityDto, Activity>();
         CreateMap<Activity, ActivityDto>()
             .ForMember(d => d.HostDisplayName, o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost)!.User.DisplayName))
-            .ForMember(d => d.HostId, o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost)!.User.Id))
-            .ForMember(d=>d.Following, o => o.MapFrom(s => s.Followers.Any(x => x.Observer.Id == currentUserId)))
-            .ForMember(d=>d.Following, o => o.MapFrom(s => s.Followers.Any(x => x.Observer.Id == currentUserId)));
+            .ForMember(d => d.HostId, o => o.MapFrom(s => s.Attendees.FirstOrDefault(x => x.IsHost)!.User.Id));
         CreateMap<ActivityAttendee, UserProfile>()
             .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.User.DisplayName))
             .ForMember(d => d.Bio, o => o.MapFrom(s => s.User.Bio))
             .ForMember(d => d.ImageUrl, o => o.MapFrom(s => s.User.ImageUrl))
             .ForMember(d => d.Id, o => o.MapFrom(s => s.User.Id))
-            .ForMember(d=>d.Following, o => o.MapFrom(s => s.User.Followers.Any(x => x.Observer.Id == currentUserId)));;
+            .ForMember(d => d.Following, o => o.MapFrom(s => s.User.Followers.Any(x => x.Observer.Id == currentUserId)))
+            .ForMember(d => d.FollowerCount, o => o.MapFrom(s => s.User.Followers.Count))
+            .ForMember(d => d.Following, o => o.MapFrom(s => s.User.Followings.Count));
         CreateMap<User, UserProfile>()
             .ForMember(d => d.FollowerCount, o => o.MapFrom(s => s.Followers.Count))
             .ForMember(d => d.Following, o => o.MapFrom(s => s.Followings.Count))
